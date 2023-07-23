a3442e6bf7260693afdd89383eae2d1199b948e2
TGYK-1117 <nikolaussires@gmail.com>
2023-07-22 23:39:53 -0500

    embedded yt video

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => 
+(
+	<iframe src='https://youtube.com/embed/WCb2xnfQKh4'
+	        frameborder='0'
+	        allow='autoplay; encrypted-media'
+	        allowfullscreen
+	        title='video'
+	/>
+)
