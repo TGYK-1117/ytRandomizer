80ee262d40711014273d65af336abad62559ed4f
TGYK-1117 <nikolaussires@gmail.com>
2023-08-08 17:57:03 -0500

    added current files. program seemingly rerenders the page infinitely. using this branch to git diff with previous working version

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -3,33 +3,34 @@ import React, { useState, useEffect } from "react";
 const embedVideo = () => {
 
 	const apiUrl = "http://localhost:3000/api/v1/videos"
-	const [ videoUrl, setVideoUrl ] = useState([])
-
-	function nextButton() {
-    	window.location.reload(false);
-  	}
-
-  	function timer() {
-  		const [startTime, setStartTime] = useState();
-  		const [now, setNow] = useState();
-
-  		setStartTime(Date.now());
-  		setNow(Date.now());
-  		setInterval(() => {
-  			setNow(Date.now())
-  		}, 1000);
-  	}
-
+	const [ videoUrl, setVideoUrl ] = useState()
+	const [ startTime, setStartTime ] = useState(null);
+	const [ now, setNow ] = useState(null);
+	const [ continueFlag, setContinueFlag ] = useState(false);
+  
 	const fetchVideo = () => {
-		fetch(apiUrl)
-			.then((response) => response.json())
-			.then((data) => {
-				setVideoUrl("https://youtube.com/embed/" + data.video_id + "?autoplay=1" + "&mute=1")
-				console.log(data)
-		})
+		fetch(apiUrl) 										//fetch from rails api access point
+			.then((response) => response.json())			//get promise returned from fetch and convert response into json 
+			.then((data) => 								//take json data and pull video_id from data, combining it into yt embed link
+				setVideoUrl("https://youtube.com/embed/"
+					+ data.video_id 
+					+ "?autoplay=1" 						//autoplay the video when embed loads
+					+ "&mute=1")							//yt policy restricts embedded videos from autoplaying unless they are muted
+			)
 	}
 
-	useEffect(() => {fetchVideo()}, [])
+	useEffect(() => {fetchVideo()}, [])	
+	useEffect(() => {
+		setStartTime(Date.now());
+		setNow(Date.now());
+		setInterval(() => {									//updates current time every 1000ms
+			setNow(Date.now())
+		}, 1000);
+	}, [])
+
+
+	if ((((now - startTime)/1000) > 5) && (continueFlag == false)) //timed refresh of the page to fetch a new video
+		{window.location.reload(false);}
 
 	return(
 		<>
@@ -42,7 +43,10 @@ const embedVideo = () => {
 				/>
 			</div>
 			<div className="pad">
-      			<button onClick={nextButton}>Next Bideo</button>
+      			<button onClick={window.location.reload(false)}>Next Bideo</button> 			
+      				{/*onClick reload the page to fetch the new video*/}
+      			{!continueFlag && <button onClick={setContinueFlag(true)}>Continue</button>}
+      				{/*onClick set flag to hide continue button once clicked*/}
     		</div>
 		</>
 	)
